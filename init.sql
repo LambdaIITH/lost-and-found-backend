ef4fadfdfeee44c50b5af49e207d4124f9eb4dd4
Suryaansh-Jain <suryaansh2003@gmail.com>
2023-02-18 16:53:21 +0530

    fix for issue #4: patch request handler for update of lost item

--- init.sql
+++ init.sql
@@ -1,16 +1,15 @@
-CREATE TABLE users (
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) PRIMARY KEY,
-  phone_number NUMERIC(10) NOT NULL,
+CREATE TABLE users ( 
+  name VARCHAR(255) NOT NULL, -- name of the user
+  email VARCHAR(255) PRIMARY KEY, -- email of the user
+  phone_number NUMERIC(10) NOT NULL UNIQUE -- phone number of the user
 );
 
 CREATE TABLE lost (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL, -- name of the item lost
   description VARCHAR(255) NOT NULL,
-  user_email VARCHAR(255) NOT NULL,
+  user_email VARCHAR(255) NOT NULL, -- email of the user who lost the item
   date_of_posting TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   status BOOLEAN NOT NULL DEFAULT FALSE, -- false = not found yet, true = found
-  FOREIGN KEY (user_email) REFERENCES users(email),
+  FOREIGN KEY (user_email) REFERENCES users(email)
 );
-
--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from dotenv import load_dotenv
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import os
+from pydantic import BaseModel
+import aiosql
+import aiohttp
+from fastapi import FastAPI, HTTPException
+import json
+
+load_dotenv()
 
 DATABASE = os.getenv("DATABASE")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
@@ -24,8 +31,45 @@ conn = psycopg2.connect(
 print("Opened database successfully!")
 
 app = FastAPI()
+queries = aiosql.from_path("./queries.sql", "psycopg2")
 
 # http://127.0.0.1:8000/
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+# http://127.0.0.1:8000/items/{item_id}
+# this is an API that lets the user who lost an item to update the status of the item 
+# will use aiosql to query the database
+@app.patch("/items/{item_id}")
+def update_item_status(item_id: int):
+    #item_id = request.match_info['item_id']
+    # update the status of the item
+
+    # gets the information of the item to be edited
+    res_raw = queries.get_item_by_id(conn, item_id=item_id)
+    
+    # checks if the item exists
+    if (res_raw == None):
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    # converts the result to a json object
+    res_get = json.dumps(res_raw, default=str)
+    res_get = json.loads(res_get)
+
+
+    if (len(res_get) == 0):
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    # swaps the curernt status of the item
+    if (res_get[0]["status"] == 1):
+        res = queries.update_item_status(conn, item_id=item_id, status = 'FALSE')
+    else:
+        res = queries.update_item_status(conn, item_id=item_id, status = 'TRUE')
+
+    conn.commit()
+
+    if (res):
+        raise HTTPException(status_code=200, detail="Status updated successfully")
+    else:
+        raise HTTPException(status_code=400, detail="Status update failed")
